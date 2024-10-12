c0d1de7489721ef9ce081afb46dd94bc8e6c2942
clementpoiret <poiret.clement@outlook.fr>
2024-10-12 16:31:27 +0200

    feat(hyprland): simplify config

--- modules/core/wayland.nix
+++ modules/core/wayland.nix
@@ -2,13 +2,16 @@
 {
   programs.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
   xdg.portal = {
     enable = true;
     wlr.enable = false;
     xdgOpenUsePortal = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
+      #pkgs.xdg-desktop-portal-hyprland
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-gtk
     ];
   };
