6f232220326dd52dfdf1a0b0c586b7d7a61a1427
clementpoiret <poiret.clement@outlook.fr>
2024-11-21 16:38:50 +0100

    feat: temporary nvidia 565.57.01 patch for linux 6.12

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -28,7 +28,18 @@ in
   services.xserver.videoDrivers = [ xserverDrivers."${host}" ];
 
   hardware.nvidia = lib.mkIf (host == "desktop") {
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    #package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "565.57.01";
+      sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
+      sha256_aarch64 = "sha256-aDVc3sNTG4O3y+vKW87mw+i9AqXCY29GVqEIUlsvYfE=";
+      openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
+      settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
+      persistencedSha256 = "sha256-hdszsACWNqkCh8G4VBNitDT85gk9gJe1BlQ8LdrYIkg=";
+      patchesOpen = [
+        ./nvidia.patch
+      ];
+    };
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
