b3a7b8819740106938e5e3fbbf84fabf6d027512
clementpoiret <poiret.clement@outlook.fr>
2025-06-20 10:19:26 +0200

    revert(zen): old flake

--- flake.nix
+++ flake.nix
@@ -44,8 +44,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    zen-browser.url = "github:clementpoiret/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
