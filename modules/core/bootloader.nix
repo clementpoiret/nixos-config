60b648aa52ca737c93b65d693d6c202924309085
clementpoiret <poiret.clement@outlook.fr>
2025-07-02 10:20:29 +0200

    hack(wl): temporary wifi7 fix

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -5,12 +5,18 @@
   pkgs,
   ...
 }:
+let
+  # To fix WiFi with Qualcomm QCNCM865 / WCN785x (must be linux 6.13.x or lower)
+  linuxPackage =
+    if (host == "laptop") then pkgs.linuxPackages_xanmod else pkgs.linuxPackages_cachyos-lto;
+  enableScx = (host != "laptop");
+in
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_cachyos-lto;
-  services.scx = {
+  boot.kernelPackages = linuxPackage;
+  services.scx = lib.mkIf enableScx {
     enable = true;
     package = pkgs.scx.full;
     scheduler = "scx_bpfland";
