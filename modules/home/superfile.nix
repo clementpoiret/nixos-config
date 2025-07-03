e1b9ab4bdb30072ff3d612d8bc787f9da886bfe2
clementpoiret <poiret.clement@outlook.fr>
2025-07-03 18:24:58 +0200

    fix(superfile): missing config

--- modules/home/superfile.nix
+++ modules/home/superfile.nix
@@ -3,25 +3,48 @@
   programs.superfile = {
     enable = true;
     settings = {
+      theme = "catppuccin-mocha";
+
+      editor = "hx";
+      dir_editor = "nvim";
+
       auto_check_update = false;
 
       default_directory = ".";
       default_sort_type = 2;
+      sort_order_reversed = false;
+      case_sensitive_sort = false;
+
+      debug = false;
+
       code_previewer = "bat";
       nerdfont = true;
+      transparent_background = true;
 
-      theme = "catppuccin-mocha";
+      zoxide_support = true;
+
+      cd_on_quit = false;
+      default_open_file_preview = true;
+      show_image_preview = true;
+      show_panel_footer_info = true;
+      file_size_use_si = false;
+      shell_close_on_success = false;
+      file_preview_width = 0;
+      sidebar_width = 20;
 
       border_top = "─";
       border_bottom = "─";
       border_left = "│";
       border_right = "│";
-      border_top_left = "┌";
-      border_top_right = "┐";
-      border_bottom_left = "└";
-      border_bottom_right = "┘";
+      border_top_left = "╭";
+      border_top_right = "╮";
+      border_bottom_left = "╰";
+      border_bottom_right = "╯";
       border_middle_left = "├";
       border_middle_right = "┤";
+
+      metadata = false;
+      enable_md5_checksum = false;
     };
   };
 }
