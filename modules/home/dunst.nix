4564ed78df8de33896e22dfeb53fa6d5427a07c4
Clément POIRET <poiret.clement@outlook.fr>
2025-09-16 16:03:57 +0200

    feat(dunst): offset

--- modules/home/dunst.nix
+++ modules/home/dunst.nix
@@ -5,6 +5,12 @@
 
     settings = {
       global = {
+        follow = "mouse";
+
+        width = 300;
+        height = "(0,300)";
+        offset = "(10,10)";
+
         corner_radius = 16;
       };
     };
