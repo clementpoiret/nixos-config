e089f28d04263e65558c713873c15184189750c3
Clément POIRET <me@int8.tech>
2025-10-14 09:15:00 +0200

    fix(nvim): use local config

--- modules/home/nvim/nvim.nix
+++ modules/home/nvim/nvim.nix
@@ -22,30 +22,30 @@
     # LSP_CODELLDB = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}";
   };
 
-  home.file = {
-    ".config/nvim/init.lua" = {
-      source = ./config/init.lua;
-    };
-    ".config/nvim/lua/options.lua" = {
-      source = ./config/lua/options.lua;
-    };
-    ".config/nvim/lua/mappings.lua" = {
-      source = ./config/lua/mappings.lua;
-    };
-    ".config/nvim/lua/chadrc.lua" = {
-      source = ./config/lua/chadrc.lua;
-    };
-    ".config/nvim/lua/configs/conform.lua" = {
-      source = ./config/lua/configs/conform.lua;
-    };
-    ".config/nvim/lua/configs/lazy.lua" = {
-      source = ./config/lua/configs/lazy.lua;
-    };
-    ".config/nvim/lua/configs/lspconfig.lua" = {
-      source = ./config/lua/configs/lspconfig.lua;
-    };
-    ".config/nvim/lua/plugins/init.lua" = {
-      source = ./config/lua/plugins/init.lua;
-    };
-  };
+  # home.file = {
+  #   ".config/nvim/init.lua" = {
+  #     source = ./config/init.lua;
+  #   };
+  #   ".config/nvim/lua/options.lua" = {
+  #     source = ./config/lua/options.lua;
+  #   };
+  #   ".config/nvim/lua/mappings.lua" = {
+  #     source = ./config/lua/mappings.lua;
+  #   };
+  #   ".config/nvim/lua/chadrc.lua" = {
+  #     source = ./config/lua/chadrc.lua;
+  #   };
+  #   ".config/nvim/lua/configs/conform.lua" = {
+  #     source = ./config/lua/configs/conform.lua;
+  #   };
+  #   ".config/nvim/lua/configs/lazy.lua" = {
+  #     source = ./config/lua/configs/lazy.lua;
+  #   };
+  #   ".config/nvim/lua/configs/lspconfig.lua" = {
+  #     source = ./config/lua/configs/lspconfig.lua;
+  #   };
+  #   ".config/nvim/lua/plugins/init.lua" = {
+  #     source = ./config/lua/plugins/init.lua;
+  #   };
+  # };
 }
