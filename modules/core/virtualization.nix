75335a52480b80b7940c48abc5c8598d4624e29b
Clément POIRET <clement@linux.com>
2026-01-29 16:01:04 +0100

    feat: virtualization

--- modules/core/virtualization.nix
+++ modules/core/virtualization.nix
@@ -1,34 +1,37 @@
 {
-  config,
   pkgs,
   username,
   ...
 }:
 {
-  # Add user to libvirtd group
-  users.users.${username}.extraGroups = [ "libvirtd" ];
-
-  # Install necessary packages
-  environment.systemPackages = with pkgs; [
-    virt-manager
-    virt-viewer
-    spice
-    spice-gtk
-    spice-protocol
-    virtio-win
-    win-spice
-    adwaita-icon-theme
-  ];
-
   # Manage the virtualisation services
   virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+    };
+
     libvirtd = {
       enable = true;
       qemu = {
         swtpm.enable = true;
       };
     };
-    spiceUSBRedirection.enable = true;
   };
-  services.spice-vdagentd.enable = true;
+
+  # Add user to libvirtd group
+  users.users.${username}.extraGroups = [
+    "libvirtd"
+    "podman"
+  ];
+
+  programs.virt-manager.enable = true;
+
+  # Install necessary packages
+  environment.systemPackages = with pkgs; [
+    dnsmasq
+    virt-viewer
+  ];
 }
