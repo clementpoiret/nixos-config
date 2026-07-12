ac7d5ed6d6fd8cdf24ebf393435edf698d78f8c1
Clément POIRET <clement@linux.com>
2026-07-12 17:33:24 +0200

    feat(jj): use helix to resolve conflicts

--- modules/home/jj.nix
+++ modules/home/jj.nix
@@ -1,6 +1,32 @@
-{ ... }:
+{ config, ... }:
+let
+  jjMergeConfig = "${config.xdg.configHome}/helix/jj-merge.toml";
+in
 {
   home.file.lesskey.text = "Q toggle-option -!^Predraw-on-quit\nq";
+
+  xdg.configFile."helix/jj-merge.toml".text = # toml
+    ''
+      [editor]
+      bufferline = "always"
+      cursorline = true
+      auto-format = false
+      auto-completion = false
+
+      [editor.lsp]
+      enable = false
+
+      [editor.statusline]
+      left = [
+        "mode",
+        "file-name",
+        "file-modification-indicator",
+      ]
+      right = [
+        "position",
+        "file-type",
+      ]
+    '';
 
   programs.jujutsu = {
     enable = true;
@@ -17,6 +43,7 @@
       git.sign-on-push = true;
       ui = {
         default-command = "log";
+        merge-editor = "hx";
 
         show-cryptographic-signatures = true;
 
@@ -26,6 +53,19 @@
           "--side-by-side"
         ];
         diff-formatter = ":git";
+      };
+      merge-tools.hx = {
+        program = "hx";
+        merge-args = [
+          "--config"
+          jjMergeConfig
+          "$output"
+          "$left"
+          "$base"
+          "$right"
+        ];
+        merge-tool-edits-conflict-markers = true;
+        conflict-marker-style = "snapshot";
       };
     };
   };
